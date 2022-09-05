fb5cbb9fdea714bcfd4fa2175ec2ff0ec4477e07
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2022-09-05 00:42:28 -0300

    Segundo commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import { router } from './routes/index'
+import cors from 'cors'
 
 export const app = express();
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/api', router)
 
